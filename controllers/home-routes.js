166e2b8d0b578f4ed78c54424fb93fa3138176c0
Ian Linton <ilinton2571@gmail.com>
2022-02-12 14:50:00 -0700

    homepage set up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,17 @@ const router = require('./api/user-routes');
 const homeroutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
+
+router.get('/', (req, res) => {
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
+});
